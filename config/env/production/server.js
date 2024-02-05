35f815c524f2464d13dc51d39f76020b86278857
prezzemolostudio <157115311+prezzemolostudio@users.noreply.github.com>
2024-02-05 13:10:25 +0100

    mod server production

---
+++ config/env/production/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    host: env('HOST', 'phplaravel-1202186-4279885.cloudwaysapps.com'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS', ["testKey1", "testKey2"]),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  });
