b42099d3cd4fbf506ebf633398ca401db90eec79
prezzemolostudio <team@prezzemolostudio.it>
2024-02-07 10:14:59 +0000

    edit env

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     host: env('HOST', 'phplaravel-1202186-4279885.cloudwaysapps.com'),
     port: env.int('PORT', 1337),
     app: {
-      keys: env.array('APP_KEYS', ["testKey1", "testKey2"]),
+      keys: env.array('APP_KEYS', [env('APP_KEY')]),
     },
     webhooks: {
       populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
